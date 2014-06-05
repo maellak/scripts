545c124839d9a2a5adcd49f5808964ea829c5dc8
Achilleas Pipinellis <axilleas@axilleas.me>
2014-06-05 20:23:55 +0300

    Script to install git

---
+++ git.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+wget -O /etc/yum.repos.d/PUIAS_6_computational.repo https://gitlab.com/gitlab-org/gitlab-recipes/raw/master/install/centos/PUIAS_6_computational.repo
+wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-puias http://springdale.math.ias.edu/data/puias/6/x86_64/os/RPM-GPG-KEY-puias
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-puias
+yum-config-manager --enable PUIAS_6_computational
+yum -y update
+git --version
