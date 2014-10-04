9212ad6543ccb3af605ce7380744735f2cb46a5f
George Fourtounis <gfour@softlab.ntua.gr>
2014-10-04 14:20:57 +0300

    Create fedora-homedir.sh

---
+++ fedora-homedir.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Configures the home directory for Apache.
+# Assumes that it has been run as 'root'.
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo Back up userdir.conf
+cp /etc/httpd/conf.d/userdir.conf /etc/httpd/conf.d/userdir.conf.backup
+
+echo Replace userdir.conf from GitHub copy
+wget https://raw.githubusercontent.com/maellak/scripts/master/userdir.conf -O /etc/httpd/conf.d/userdir.conf
+
+echo Restart Apache
+service httpd restart
+
+echo Generate and set up public folder for ellak user
+mkdir /home/ellak/public_html
+chmod 711 /home/ellak
+chown ellak:ellak /home/ellak/public_html
+chmod 755 /home/ellak/public_html
+
+echo Set SELinux policies
+setsebool -P httpd_enable_homedirs true
+chcon -R -t httpd_sys_content_t /home/ellak/public_html
